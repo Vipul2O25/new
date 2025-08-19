d29acd11a8b091e7ab1d0455adbdd7bbd8074447
Vipul2O25 <vipulsodhaprmar2022@student.aau.in>
2025-08-19 22:33:43 +0530

    learn

--- index.php
+++ index.php
@@ -1,4 +1,52 @@
 <?php
-    echo"hello";
-    print("hello");
+
+    // for($i = 1; $i<=10; $i++)
+    // {
+    //     printf("\nalpavipul");
+    // }
+
+
+    // $s =1;
+    // while($s<=10)
+    // {
+    //     echo"$s";
+    //     $s++;
+    // }
+
+    // $s =11;
+    // do
+    // {
+    //    echo $s . "<br>";
+    //     $s++;
+    // }
+    // while($s <=20)
+
+
+// $fruits = [1,2,3,4,5,6,7,8,9,0,7,6,5,4,3,2,2,34,5];
+
+// foreach ($fruits as $fruit) {
+//     echo $fruit . "<br>";
+// }
+
+
+ 
+
+for ($i = 1; $i <= 5; $i++) {
+    // print spaces
+    for ($j = 5; $j > $i; $j--) {
+        echo "&nbsp;&nbsp;";
+    }
+    // print stars
+    for ($k = 1; $k <= 2 * $i - 1; $k++) {
+        echo "*";
+    }
+    echo "<br>";
+}
+
+
+
+
+
+
+
 ?>
---
+++ login_action.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+ $_SESSION['username'] = $username;
+
+header('Location: dashboard.php');
+
+
+
+
+?>
+
